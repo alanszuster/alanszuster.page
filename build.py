13658731f2fc003afa4ada67b66762e60c9636a3
Alan Szuster <alan.szuster@sallinggroup.com>
2025-06-24 20:45:00 +0200

    update page &  auto deployment

--- build.py
+++ build.py
@@ -1,6 +1,10 @@
 from flask_frozen import Freezer
 from app import app
+import os
+import shutil
 
+# Configure Freezer to use the root directory
+app.config['FREEZER_DESTINATION'] = '.'
 freezer = Freezer(app)
 
 @freezer.register_generator
@@ -11,4 +15,13 @@ def url_generator():
     yield 'contact', {}
 
 if __name__ == '__main__':
+    # Clean previously generated files if they exist
+    paths_to_clean = ['about', 'contact', 'skills', 'index.html']
+    for path in paths_to_clean:
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        elif os.path.exists(path):
+            os.remove(path)
+
+    # Generate new static files
     freezer.freeze()
