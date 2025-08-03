863320659dc039799beb32cd28c1121d5cd9fb57
Alan Szuster <alan.szuster@sallinggroup.com>
2025-08-03 16:41:33 +0200

    Add Jest configuration file for testing setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testMatch: [
+    "<rootDir>/src/**/*.test.[jt]s?(x)",
+    "<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)",
+  ],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+};
